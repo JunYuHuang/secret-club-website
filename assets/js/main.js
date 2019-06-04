6ff4ef24202ad4bd1041f4dc0cfa86454e917cf5
Jun Yu Huang <junyu_huang@hotmail.com>
2019-06-04 15:31:58 -0700

    update home-splash-bg and finish about section

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,16 @@ var currentYears = document.getElementsByClassName("currentYear");
 for (var i = 0; 0 < currentYears.length; i++) {
    currentYears[i].innerHTML = new Date().getFullYear();
 }
+
+// change navbar background color from transparent to black after scrolling past #home-splash-background
+var fixedTopNavbar = document.getElementById("fixedTopNavbar");
+window.onscroll = function() {
+    "use strict";
+    if (document.body.scrollTop >= 10 ) {
+        fixedTopNavbar.classList.add("bg-black");
+        fixedTopNavbar.classList.remove("bg-transparent");
+    } else {
+        fixedTopNavbar.classList.add("bg-transparent");
+        fixedTopNavbar.classList.remove("bg-black");
+    }
+};
