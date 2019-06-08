da83cd06a53870b6bb4788bf75615de64cb4c7c6
Jun Yu Huang <junyu_huang@hotmail.com>
2019-06-07 19:01:09 -0700

    add partial lazy loaded images
    -make about section image lazy loaded in future updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,7 @@ $('.navbar-collapse a').click(function(){
 
 var currentYear = document.getElementById("currentYear");
 currentYear.innerHTML = new Date().getFullYear();
+
 // change navbar background color from transparent to black after scrolling past #home-splash-background
 // var fixedTopNavbar = document.getElementById("fixedTopNavbar");
 // window.onscroll = function() {
@@ -17,3 +18,28 @@ currentYear.innerHTML = new Date().getFullYear();
 //         fixedTopNavbar.classList.remove("bg-black");
 //     }
 // };
+
+// lazy loading sourced from https://www.youtube.com/watch?v=aUjBvuUdkhg
+const targets = document.querySelectorAll('img');
+
+const lazyLoad = target => {
+    const io = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            //
+
+            if (entry.isIntersecting) {
+                const img = entry.target;
+                const src = img.getAttribute('data-lazy');
+
+                img.setAttribute('src', src);
+                img.classList.add('img-fade');
+
+                observer.disconnect();
+            }
+        });
+    });
+
+    io.observe(target);
+}
+
+targets.forEach(lazyLoad);
