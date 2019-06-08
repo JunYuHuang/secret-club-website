2c8a31dcc8d8cf692adb077120930c7cdc98b641
Jun Yu Huang <junyu_huang@hotmail.com>
2019-06-07 23:41:50 -0700

    attempt to fix mobile issues with negative margins

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,25 +21,18 @@ currentYear.innerHTML = new Date().getFullYear();
 
 // lazy loading sourced from https://www.youtube.com/watch?v=aUjBvuUdkhg
 const targets = document.querySelectorAll('img');
-
 const lazyLoad = target => {
     const io = new IntersectionObserver((entries, observer) => {
         entries.forEach(entry => {
-            //
-
             if (entry.isIntersecting) {
                 const img = entry.target;
                 const src = img.getAttribute('data-lazy');
-
                 img.setAttribute('src', src);
                 img.classList.add('img-fade');
-
                 observer.disconnect();
             }
         });
     });
-
     io.observe(target);
 }
-
 targets.forEach(lazyLoad);
