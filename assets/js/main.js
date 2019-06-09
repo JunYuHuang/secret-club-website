d535e6544f5b8b2d8c84a20026eaf199c97f43fb
Jun Yu Huang <junyu_huang@hotmail.com>
2019-06-08 20:30:17 -0700

    make navbar change from transparent to black after scrolling past splash section

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,17 +7,13 @@ var currentYear = document.getElementById("currentYear");
 currentYear.innerHTML = new Date().getFullYear();
 
 // change navbar background color from transparent to black after scrolling past #home-splash-background
-// var fixedTopNavbar = document.getElementById("fixedTopNavbar");
-// window.onscroll = function() {
-//     "use strict";
-//     if (document.body.scrollTop >= 1000 ) {
-//         fixedTopNavbar.classList.add("bg-black");
-//         fixedTopNavbar.classList.remove("bg-transparent");
-//     } else {
-//         fixedTopNavbar.classList.add("bg-transparent");
-//         fixedTopNavbar.classList.remove("bg-black");
-//     }
-// };
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $("#fixedTopNavbar");
+        var $parallaxBackground = $("#home-splash");
+        $nav.toggleClass("bg-black", $(this).scrollTop() > $parallaxBackground.height());
+    });
+});
 
 // lazy loading sourced from https://www.youtube.com/watch?v=aUjBvuUdkhg
 const targets = document.querySelectorAll('img');
